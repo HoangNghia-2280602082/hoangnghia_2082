e6b3d6eef956a29566604ea821cb92318950b85c
hoang nghia <nghiatran56714@gmail.com>
2026-03-09 20:21:56 +0700

    update main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+def say_hello():
+    print("Hello GitHub 🚀")
+    print("This is my Home Task 1")
+
+
+def sum_two_numbers(a, b):
+    return a + b
+
+
+if __name__ == "__main__":
+    say_hello()
+
+    result = sum_two_numbers(5, 7)
+    print("Sum:", result)
